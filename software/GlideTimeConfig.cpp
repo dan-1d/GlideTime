af5309c6ec81e70c58cf21ab3543041c524e7c2a
Dan <dan.gebhardt@gmail.com>
2019-05-24 17:00:50 -0700

    Hardcoded default calibration
    Hardcoded default calibration adjusted in consts.h file.

--- software/GlideTimeConfig.cpp
+++ software/GlideTimeConfig.cpp
@@ -6,12 +6,14 @@
 GlideTimeConfig::set_clock_cal(){
   // calculate the number of millis to wait between adjustments.
   // ensure the millis value is positive, and save the direction to  clock_cal_direction
-  long ms_diff_per_minute;
-  EEPROM.get(EEPROM_ADDR_CALIBRATION_DATA, ms_diff_per_minute);
-  if( ms_diff_per_minute != 0 ){
-    ms_diff_per_minute = ms_diff_per_minute < 0 ? -ms_diff_per_minute : ms_diff_per_minute;
-    clock_cal_millis_per_correction = 60000 / ms_diff_per_minute;
-    clock_cal_direction = ms_diff_per_minute > 0 ? 1 : -1;  // increment or decrement depending on if clock is fast or slow
+//  long ms_diff_per_minute;
+//  EEPROM.get(EEPROM_ADDR_CALIBRATION_DATA, ms_diff_per_minute);
+//  if( ms_diff_per_minute != 0 ){
+//    ms_diff_per_minute = ms_diff_per_minute < 0 ? -ms_diff_per_minute : ms_diff_per_minute;
+//    clock_cal_millis_per_correction = 60000 / ms_diff_per_minute;
+//    clock_cal_direction = ms_diff_per_minute > 0 ? 1 : -1;  // increment or decrement depending on if clock is fast or slow
+    clock_cal_millis_per_correction = DEFAULT_CLOCK_CALIBRATION_MS_PER_CORRECTION;
+    clock_cal_direction = DEFAULT_CLOCK_CALIBRATION_DIRECTION > 0 ? 1 : -1;  // increment or decrement depending on if clock is fast or slow
   }
 
 /*
